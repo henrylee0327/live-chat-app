91b6c1a403391bfbc0c1e7b03999275573c0796e
henry lee <djteski@gmail.com>
2020-09-13 22:41:46 -0500

    final commit

--- server/server.js
+++ server/server.js
@@ -13,11 +13,6 @@ const port = process.env.PORT || 5000
 app.use(express.static('../client/build'))
 app.use(cors())
 
-
-// app.get('/', (req, res) => {
-//     res.render('index.html')
-// })
-
 app.get('/', (req, res) => {
     res.send('server is up and running').status(200)
 })
